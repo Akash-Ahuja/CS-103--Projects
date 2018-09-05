984b8d529215891f1863dcfcc031011d0bf53e4d
William E. Skeith <wes@cs.ccny.cuny.edu>
2018-09-05 17:20:00 -0400

    First project.

---
+++ p1/hello.cpp
@@ -0,0 +1,13 @@
+/*
+ * CSc103 Project 1: (hello_world++)
+ * See readme.html for details.
+ * Please list all references you made use of in order to complete the
+ * assignment: your classmates, websites, etc.  Aside from the lecture notes
+ * and the book, please list everything.  And remember- citing a source does
+ * NOT mean it is okay to COPY THAT SOURCE.  What you submit here **MUST BE
+ * YOUR OWN WORK**.
+ * References:
+ *
+ */
+
+
