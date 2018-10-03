9e5eb1de3ba937af393ac615f29fa47e0a1f6df5
William E. Skeith <wes@cs.ccny.cuny.edu>
2018-10-03 16:04:19 -0400

    Adding explicit directory for the warm-up.

---
+++ p3/warm-up/warmup.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+using std::vector;
+#include <iostream>
+using std::cout;
+
+vector<vector<bool> > world = {
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+};
+
+int main()
+{
+	/* NOTE: the vector 'world' above corresponds to the contents
+	 * of ../res/tests/0.  TODO: apply the rules to the vector,
+	 * write the result to standard output, and compare with the
+	 * contents of ../res/tests/1. */
+	return 0;
+}
